ec95e0c3ffce073a65c6278a14f51cc4bbd059e8
yixuanjia <yixuanj3@illinois.edu>
2019-04-24 12:07:12 -0500

    III

---
+++ app/src/main/java/com/example/cs125finalproject/Start.java
@@ -0,0 +1,27 @@
+package com.example.cs125finalproject;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Start extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+        configureNextButton();
+
+    }
+    private void configureNextButton() {
+        final Button startActivity = (Button) findViewById(R.id.Start);
+        startActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Start.this, MainActivity.class));
+            }
+        });
+    }
+}
